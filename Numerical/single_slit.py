e55fb13ea3a72da4237991b327a7de514f28193e
Nicole Sze <2852823s@brutha1.physics.gla.ac.uk>
2026-01-29 12:45:18 +0000

    single slit done

--- Numerical/single_slit.py
+++ Numerical/single_slit.py
@@ -40,7 +40,7 @@ def wavepacket(t, dt, dx, a, b, ky):
     psi_n = np.zeros((Nx, Nx), dtype = complex)
     for i, xi in enumerate(grid_x):
         for j, xj in enumerate(grid_y):
-            psi_n[i, j] = (2*a/np.pi)**0.25*(2*b/np.pi)**0.25*np.exp(-a*xi**2-b*xj**2)*np.exp(1j*ky*xj)  # Initial condition
+            psi_n[i, j] = (2*a/np.pi)**0.25*(2*b/np.pi)**0.25*np.exp(-a*(xi+2)**2-b*xj**2)*np.exp(1j*ky*(xi+2))  # Initial condition
     psi_n[0, :] = psi_n[-1, :] = 0  # Boundary condition
     psi_n[:, 0] = psi_n[:, -1] = 0  # Boundary condition
 
@@ -54,17 +54,20 @@ def wavepacket(t, dt, dx, a, b, ky):
     # x_1 = centre of barrier(units of dx)
     # x_2 = width of barrier(units of dx and even number)
     # y_1 = width of barrier in y (units of dx)
-    x_1 = Nx/2.0
+    x_1 = np.round(Nx/2.0,0)
     x_2 = 8
     y_1 = 8
     # Loops through each space index and inserts corresponding potential
-    v = np.zeros(N)
-    for i in range(N):
-        if i < x_1 - x_2/2 or i > x_1 + x_2/2:
-            if i > y_1 - x_2/2 or i < y_1 + x_2/2:
-                v[i] = 0
-        else:
-            v[i] = 2000000000
+    v = np.zeros((Nx, Nx))
+    for i in range(Nx):
+        for j in range(Nx):
+            if j > x_1 - y_1/2.0 and j < x_1 + y_1/2.0:
+                if i > x_1 - x_2/2.0 and i < x_1 + x_2/2.0:
+                    v[j,i] = 0
+                else:
+                    v[j,i] = 2000000000
+
+    v = v.flatten()
 
     # Setting up matrix A
     array_A = np.zeros((N,N),dtype=complex)
@@ -72,6 +75,7 @@ def wavepacket(t, dt, dx, a, b, ky):
         for r in range(N):
             if s == r:
                 array_A[s,r] = complex(1+1j*alpha+1j*alpha) + 1j*dt*v[r]/2
+                #print(array_A[s,r])
             elif s == r + 1 or s == r - 1:
                 array_A[s,r] = complex(-1j*alpha/2)
             elif s == r + Nx or s == r - Nx:
@@ -82,12 +86,13 @@ def wavepacket(t, dt, dx, a, b, ky):
     for n in range(N):
         for m in range(N):
             if n == m:
-                array_B[n,m] = complex(1-1j*alpha-1j*alpha) - 1j*dt*v[r]/2
+                array_B[n,m] = complex(1-1j*alpha-1j*alpha) - 1j*dt*v[m]/2
+                #print(array_B[n,m])
             elif n == m + 1 or n == m - 1:
                 array_B[n,m] = complex(1j*alpha/2)
             elif n == m + Nx or n == m - Nx:
                 array_B[n,m] = complex(1j*alpha/2)
-
+        
     psi_n1 = psi_n.copy()
     
     for k in range(Nt):
@@ -99,21 +104,24 @@ def wavepacket(t, dt, dx, a, b, ky):
     
     return (psi_n1, grid_x, grid_y)
 
-inputs = list(map(float, input('Enter time period (t), time step (dt), grid spacing (x direction) (dx), normalised Gaussian width (x direction) (a), normalised Gaussian width (y direction) (b) and wave number in y direction (ky): ').split()))
+inputs = list(map(float, input('Last time, time step (dt), grid spacing (x direction) (dx), normalised Gaussian width (x direction) (a), normalised Gaussian width (y direction) (b) and wave number in y direction (ky): ').split()))
+
+n=9
+times = np.arange(0,inputs[0],inputs[0]/n)
 
 # 3D plot
-psi_n1, grid_x, grid_y = wavepacket(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5])
-x, y = np.meshgrid(grid_x, grid_y)
-z = np.abs(psi_n1)**2
-
 fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0)
-ax.set_xlabel('Position (x)')
-ax.set_ylabel('Position (y)')
-ax.set_zlabel('(|ψ|^2)')
-ax.set_zlim([0, 0.85])
-plt.title("t = "+str(inputs[0]))
+for counter in range(n):
+    psi_n1, grid_x, grid_y = wavepacket(times[counter], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5])
+    x, y = np.meshgrid(grid_x, grid_y)
+    z = np.abs(psi_n1)**2
+    ax = fig.add_subplot(int(np.sqrt(n)),int(np.sqrt(n)), counter +1, projection="3d")
+    ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0)
+    ax.set_xlabel('Position (x)')
+    ax.set_ylabel('Position (y)')
+    ax.set_zlabel('(|ψ|^2)')
+    ax.set_zlim([0, 0.85])
+    plt.title("t = "+str(round(times[counter],2)))
 plt.tight_layout()
-plt.savefig('single_slit'+str(inputs[0])+'.pdf')
+plt.savefig('single_slit9times.pdf')
 plt.show()
